25111d78319190d5b27090cfd70c0d0416c2a859
EndeyshentLabs <Themikfound@gmail.com>
2023-03-03 14:49:39 +0300

    Fixed `install.sh`

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-printf "[NOTE] RUN WITH SUDO!"
+printf "[NOTE] RUN WITH SUDO!\n"
 
-ln -sf /usr/local/bin/ "$SCRIPT_DIR/fluid-init"
+read -s -p "Press any key to continue... " -n 1
+printf "\n"
+
+ln -sf "$SCRIPT_DIR/fluid-init" /usr/local/bin/
 
 # vim:set noet sw=4 ts=4 ft=zsh:
